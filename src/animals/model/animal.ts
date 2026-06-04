b9fb6d550ecbd71a485cd9e5e02f670c16f26b94
Franco Durand <francoduve.25@gmail.com>
2026-06-04 23:55:31 +0000

    feat(animals): add thresholds for temperature and heart rate in animal model and forms

--- src/animals/model/animal.ts
+++ src/animals/model/animal.ts
@@ -6,6 +6,10 @@ export class Animal {
     breed: string = '';
     bovineImg?: File | string;
     stableId: number = 0;
+    minTemperature: number = 38.0;
+    maxTemperature: number = 39.3;
+    minHeartRate: number = 40;
+    maxHeartRate: number = 80;
 
     constructor(data: Partial<Animal> = {}) {
         Object.assign(this, data);
